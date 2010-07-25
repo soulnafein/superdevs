58230f81fafd94e7e34e36bb7af270da99bba373
David Santoro <soulnafein@gmail.com>
2010-07-25 14:50:38 +0100

    Sendmail configuration is valid for production now

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,9 +17,5 @@ Superdevs::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.sendmail_settings = {
-          :location => '/usr/sbin/sendmail'
-  }
 
 end
