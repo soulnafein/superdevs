36ff85f029594b602ee5905531c32489efb385cf
David Santoro <soulnafein@gmail.com>
2010-07-25 14:24:24 +0100

    Playing around with logging configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,14 @@
 set :application, "superdevs"
-set :repository,  "git@github.com:soulnafein/superdevs.git"
 set :user, "dsantoro"
-set :scm_username, "soulnafein@gmail.com"
 default_run_options[:pty] = true
 
+set :repository,  "git@github.com:soulnafein/superdevs.git"
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm_username, "soulnafein@gmail.com"
 
 role :web, "superdevs.com"                          # Your HTTP server, Apache/etc
 role :app, "superdevs.com"                          # This may be the same as your `Web` server
 role :db,  "superdevs.com", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
-
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
 
 namespace :deploy do
   task :start do ; end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,4 @@
 Superdevs::Application.configure do
-  # Settings specified here will take precedence over those in config/environment.rb
-
-  # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -10,16 +7,13 @@ Superdevs::Application.configure do
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # For nginx:
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-
-  # If you have no front-end server that supports something like X-Sendfile,
-  # just comment this out and Rails will serve the files
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+   config.log_level = :debug
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
