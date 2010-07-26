3ff18807a7bba2e5669e5173480dd42edea0bd56
David Santoro <soulnafein@gmail.com>
2010-07-26 13:24:43 +0100

    Fixed deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ namespace :deploy do
 
   desc "run 'bundle install' to install Bundler's packaged gems for the current deploy"
   task :bundle_install, :roles => :app do
-    run "cd #{release_path} && bundle install"
+    run "cd #{release_path} && sudo -i bundle install"
   end
 end
 
