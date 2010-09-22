f07cf1fc77dff54e6f2b019e9c44b6fdccd67451
David Santoro <soulnafein@gmail.com>
2010-09-22 21:41:58 +0100

    FIxed broken spec

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe EventsController do
+  before :each do
+    UserSession.stub(:find).and_return(mock_session)
+  end
   describe "GET 'show'" do
     it "should load the event from the database" do
       event = mock(:event)
@@ -17,5 +20,15 @@ describe EventsController do
 
       response.status.should == 404
     end
+  end
+
+  def mock_session
+    session = mock(UserSession).as_null_object
+    session.stub(:record).and_return(mock_user)
+    @mock_session ||= session
+  end
+
+  def mock_user(stubs={})
+    @mock_user ||= mock_model(User, stubs).as_null_object
   end
 end
