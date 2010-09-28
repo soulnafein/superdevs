585194b5d4392114404be026f0989326b56f7b58
David Santoro <soulnafein@gmail.com>
2010-09-28 18:17:28 +0100

    Refactored routes to be rails 3.1 compatible

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,28 @@
-Superdevs::Application.routes.draw do |map|
-  resources :examples
+Superdevs::Application.routes.draw do
+  resources :user_sessions, :users, :events, :attendances, :password_resets
 
-  resources :user_sessions
-  resources :users
-  resources :events
-  resources :attendances
-  map.follow_user 'users/:id/follow', :controller => 'users', :action => 'follow', :method => :put
-  resources :password_resets
-  map.activate '/activate/:activation_code', :controller => 'activations', :action => 'create'
-  map.login 'login', :controller => 'user_sessions', :action => 'new'
-  map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'
-  map.register 'register', :controller => 'users', :action => 'new'
-  map.addrpxauth "addrpxauth", :controller => "users", :action => "addrpxauth", :method => :post
-  map.complete_registration "complete_registration", :controller => "users", :action => "complete_registration", :method => :put
-  map.edit_mandatory_details "edit_mandatory_details", :controller => "users", :action => "edit_mandatory_details", :method => :get
+  match 'users/:id/follow', :to => 'users#follow', :as => 'follow_user',
+          :via  => :put
 
-  map.invitation_requested 'invitation_requested', :controller => 'pages', :action => 'invitation_requested'
-  map.privacy_policy 'privacy_policy', :controller => 'pages', :action => 'privacy_policy'
-  map.terms_and_conditions 'terms_and_conditions', :controller => 'pages', :action => 'terms_and_conditions'
-  map.contact_us 'contact_us', :controller => 'pages', :action => 'contact_us'
+  controller :user_sessions do
+    match '/login', :to => :new, :as => 'login'
+    match '/logout', :to => :destroy, :as => 'logout'
+  end
+
+  controller :users do
+    match 'addrpxauth', :to => :addrpxauth, :as => 'addrpxauth', :via => :post
+    match 'complete_registration', :to => :complete_registration, :via => :put
+    match 'edit_mandatory_details', :to => :edit_mandatory_details
+  end
+
+  controller :pages do
+    match 'invitation_requested', :to => :invitation_requested
+    match 'privacy_policy', :to => :privacy_policy
+    match 'terms_and_conditions', :to => :terms_and_conditions
+    match 'contact_us', :to => :contact_us
+  end
 
   get "home/index"
-  get "pages/invitation_requested"
 
   root :to => "home#index"
 end
