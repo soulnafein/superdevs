3e2f5b23b532dab169f5d9cec507e07662aeacb7
Ken Alex Fassone <nexusventuri@gmail.com>
2010-10-03 11:49:39 +0200

    removed annotation from event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,18 +1,3 @@
-# == Schema Information
-#
-# Table name: events
-#
-#  id          :integer         not null, primary key
-#  title       :string(255)
-#  city        :string(255)
-#  country     :string(255)
-#  description :text
-#  link        :string(255)
-#  created_at  :datetime
-#  updated_at  :datetime
-#  date        :datetime
-#
-
 class Event < ActiveRecord::Base
   has_many :attendances
   has_many :attendees, :through => :attendances, :source => :user
