21c89c6c1a4b8731c9be40b4871e0fc2e65de035
David Santoro <soulnafein@gmail.com>
2010-11-07 18:00:42 +0000

    Fixing problem after deployment

--- app/models/event_behaviours/queries.rb
+++ app/models/event_behaviours/queries.rb
@@ -1,33 +1,33 @@
 module EventBehaviours
   module Queries
-    def self.exists?(event)
+    def exists?(event)
       Event.where("title = ? and date = ? and country = ?",
                   event.title, event.date, event.country).size > 0
     end
 
-    def self.get_events_for_user(user)
+    def get_events_for_user(user)
       events = get_events_for_user_city(user)
       events = get_events_for_user_country(user) if events.empty?
       events
     end
 
-    def self.get_events_for_user_city(user)
+    def get_events_for_user_city(user)
       get_events_for_user_country(user).where("upper(city) = ?", user.city.to_s.upcase)
     end
 
-    def self.get_events_for_user_country(user)
+    def get_events_for_user_country(user)
       Event.all_upcoming.where("upper(country) = ?", user.country.to_s.upcase)
     end
 
-    def self.all_upcoming
+    def all_upcoming
       Event.where("date > ?", Time.now).order("date ASC")
     end
 
-    def self.united_kingdom
+    def united_kingdom
       Event.all_upcoming.where("upper(country) = 'UNITED KINGDOM'")
     end
 
-    def self.all_past
+    def all_past
       Event.where("date < ?", Time.now).order("date ASC")
     end
   end
