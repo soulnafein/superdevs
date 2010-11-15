359a8374db43e1fee63d2dc8e4b864024e87da55
Ken Alex Fassone <nexusventuri@gmail.com>
2010-11-15 19:55:09 +0100

    removed deprecated methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -88,17 +88,17 @@ class User < ActiveRecord::Base
 
   def deliver_activation_instructions!
     reset_perishable_token!
-    Notifier.deliver_activation_instructions(self)
+    Notifier.activation_instructions(self).deliver
   end
 
   def deliver_welcome!
     reset_perishable_token!
-    Notifier.deliver_welcome(self)
+    Notifier.welcome(self).deliver
   end
 
   def deliver_password_reset_instructions!
     reset_perishable_token!
-    Notifier.deliver_password_reset_instructions(self)
+    Notifier.password_reset_instructions(self).deliver
   end
 
   include UserBehaviours::TwitterStyleFollowBehaviour
