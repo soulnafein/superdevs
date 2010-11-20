01b2bd84f62c70d088a37b76646eb210bbfe0c93
Ken Alex Fassone <nexusventuri@gmail.com>
2010-11-20 12:41:11 +0100

    variable name refactoring

--- lib/tasks/email_notifications/lib/tomorrow_event_notification.rb
+++ lib/tasks/email_notifications/lib/tomorrow_event_notification.rb
@@ -1,8 +1,8 @@
 class TomorrowEventNotification
   def self.execute
-    next_week = Date.today + 1
+    tomorrow = Date.today + 1
 
-    events = Event.where(["date >= ? AND date < ?", next_week, next_week + 1])
+    events = Event.where(["date >= ? AND date < ?", tomorrow, tomorrow + 1])
     events.each do |event| 
       event.attendees.each do |user|
          Notifier.tomorrow_event_notification(user, event).deliver
