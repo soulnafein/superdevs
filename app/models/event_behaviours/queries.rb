5f06be8ac5be2354d6a1d06cf2b6f726c6d4c6ef
David Santoro <soulnafein@gmail.com>
2010-12-02 12:52:03 +0000

    Fixed list of events for group and highlighted today events

--- app/models/event_behaviours/queries.rb
+++ app/models/event_behaviours/queries.rb
@@ -25,7 +25,7 @@ module EventBehaviours
     end
 
     def all_upcoming
-      Event.where("date > ?", Time.now.beginning_of_day).order("date ASC")
+      Event.where("date >= ?", Time.now.beginning_of_day).order("date ASC")
     end
 
     def united_kingdom
@@ -33,7 +33,7 @@ module EventBehaviours
     end
 
     def all_past
-      Event.where("date < ?", Time.tomorrow).order("date ASC")
+      Event.where("date < ?", Time.now.beginning_of_day).order("date ASC")
     end
   end
 end
